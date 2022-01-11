a92143bad1f4752f17601c0fe15292a2c8524edc
Raghav Prabhakar <raghavprabhakar66@gmail.com>
2022-01-11 21:12:07 +0530

    add avg fps counter

--- src/utils/display.py
+++ src/utils/display.py
@@ -3,7 +3,6 @@ import time
 import cv2
 
 from src.detection.detector import Detectors
-import matplotlib.pyplot as plt
 
 FONT = cv2.FONT_HERSHEY_SIMPLEX
 FONT_SCALE = 0.5
@@ -11,13 +10,18 @@ FONT_COLOR = (255, 0, 0)
 LINETYPE = 2
 
 
-def display_video(filepath=None, resize_shape=None, scale=None, model='Mediapipe'):
+def display_video(
+    filepath=None,
+    resize_shape=None,
+    scale=None,
+    model="Mediapipe",
+    extract_face=False,
+):
     detector = Detectors().loadModel(model)
-    if filepath is None:
-        cap = cv2.VideoCapture(0)
-    else:
-        cap = cv2.VideoCapture(filepath)
-    prev_frame_time = curr_frame_time = curr_frame_id = 0
+    cap = (
+        cv2.VideoCapture(0) if filepath is None else cv2.VideoCapture(filepath)
+    )
+    prev_frame_time = curr_frame_time = curr_frame_id = a = 0
 
     while cap.isOpened():
         ret, frame = cap.read()
@@ -27,25 +31,31 @@ def display_video(filepath=None, resize_shape=None, scale=None, model='Mediapipe
 
         height, width, _ = frame.shape
         curr_frame_time = time.time()
-        fps = int(1 / (curr_frame_time - prev_frame_time))
+        fps = round(1 / (curr_frame_time - prev_frame_time))
+        a += fps
         prev_frame_time = curr_frame_time
 
         if resize_shape is not None:
             frame = cv2.resize(frame, resize_shape)
         elif scale is not None:
-            frame = cv2.resize(frame, (int(width * scale), int(height * scale)))
+            frame = cv2.resize(
+                frame, (int(width * scale), int(height * scale))
+            )
 
-        if curr_frame_id % 1 == 0:
+        if curr_frame_id % 5 == 0:
             results = detector.detect(frame)
-        
+
         grid = []
         for bbox in results:
             x, y, w, h = bbox
-            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            face = cv2.resize(frame[y:y+h, x:x+w], (256, 256))
-            grid.append(face)
+            if extract_face:
+                face = cv2.resize(
+                    frame[y - 50 : y + h + 50, x - 50 : x + w + 50], (256, 256)
+                )
+                grid.append(face)
+            else:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
         faces = cv2.hconcat(grid)
-
 
         cv2.putText(
             frame,
@@ -56,8 +66,9 @@ def display_video(filepath=None, resize_shape=None, scale=None, model='Mediapipe
             FONT_COLOR,
             LINETYPE,
         )
+
         if faces is not None:
-            cv2.imshow("frame", faces)
+            cv2.imshow("faces", faces)
         else:
             cv2.imshow("frame", frame)
         curr_frame_id += 1
@@ -65,5 +76,6 @@ def display_video(filepath=None, resize_shape=None, scale=None, model='Mediapipe
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
 
+    print(f"Average FPS: {a//curr_frame_id}")
     cap.release()
     cv2.destroyAllWindows()
