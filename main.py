64dc5dafc5300bdd605130d0a9fc100cacd7c108
Raghav Prabhakar <raghavprabhakar66@gmail.com>
2022-01-12 21:38:46 +0530

    add argparse

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+import argparse
 import os
+from pprint import pprint
 
 from src.utils.display import display_video
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d",
+        "--detector",
+        default="Mediapipe",
+        help="model used for face detection",
+    )
+    parser.add_argument(
+        "-e",
+        "--extract-face",
+        action="store_true",
+        default=False,
+        help="extract faces from video",
+    )
+    parser.add_argument(
+        "-a",
+        "--align-face",
+        action="store_true",
+        default=False,
+        help="align extracted faces from video",
+    )
+    args = parser.parse_args()
     videos = os.listdir("dataset/test/detection/videos")
+    pprint(args)
     display_video(
         filepath="dataset/test/detection/videos/" + videos[4],
-        model="Mediapipe",
-        extract_face=False,
+        model=args.detector,
+        extract_face=args.extract_face,
+        align_face=args.align_face,
     )
