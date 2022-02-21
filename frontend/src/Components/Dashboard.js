4f094f150ccef5eeeb1a148caca219b23195caf0
Kabir <seth.kabir14@gmail.com>
2022-02-21 23:43:00 +0530

    customize menu button

--- frontend/src/Components/Dashboard.js
+++ frontend/src/Components/Dashboard.js
@@ -6,9 +6,7 @@ const Dashboard = () => {
 	return (
 		<div className="flex flex-col h-screen">
 			<Navbar />
-
-
-			<footer className="absolute bottom-5 right-5">
+			<footer className="absolute bottom-5 right-5 z-10">
 				<Link to="/add-student">
 					<button className="flex h-20 w-20 bg-blue-200 rounded-full transition-all active:w-[4.9rem] active:h-[4.9rem] active:bg-blue-100">
 						<UserAddIcon className="h-10 w-10 m-auto" />
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,19 +1,72 @@
-import { MenuIcon } from "@heroicons/react/solid";
+import { MenuIcon, HomeIcon, UserAddIcon, LogoutIcon } from "@heroicons/react/solid";
+import { Menu, Transition } from '@headlessui/react'
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
 	return (
-		<div className="navbar mb-2 shadow-lg bg-blue-500 text-neutral-content ">
-			<div className="flex-1 px-2 mx-2">
-				<span className="text-lg font-bold">
-					Thapar Attendence System 
-				</span>
+		<div>
+			<div className="navbar mb-2 shadow-lg bg-blue-500 text-neutral-content ">
+				<div className="flex-1 px-2 mx-2">
+					<span className="text-lg font-bold">
+						Thapar Attendence System
+					</span>
+				</div>
 			</div>
-			<div className="flex-none">
-				<button className="btn btn-square btn-ghost">
-					<MenuIcon className="h-8 w-8" />
-				</button>
+			<div className="absolute right-0 top-0 p-2 z-20">
+				<Menu>
+					<Menu.Button className="ml-32">
+						<button className="btn btn-square btn-ghost">
+							<MenuIcon className="h-8 w-8" />
+						</button>
+					</Menu.Button>
+
+					<Menu.Items className="flex flex-col space-y-2 mt-4 bg-slate-200 rounded-lg p-2 divide-y w-44">
+						<Link to="/dashboard">
+							<Menu.Item className="flex justify-start hover:bg-blue-200 p-1 rounded-lg">
+								{({ active }) => (
+
+									<div
+										className={`${active && 'bg-blue-500 flex flex-col'}`}
+									>
+										<HomeIcon className="h-5 w-5 mr-1" />
+										Home
+									</div>
+
+								)}
+							</Menu.Item>
+						</Link>
+						<Link to="/add-student">
+							<Menu.Item className="flex justify-start hover:bg-blue-200 p-1 rounded-lg">
+								{({ active }) => (
+									<div
+										className={`${active && 'bg-blue-500'}`}
+									>
+										<UserAddIcon className="h-5 w-5 mr-1" />
+										Add Student
+									</div>
+								)}
+							</Menu.Item>
+						</Link>
+						<Link to="/login">
+							<Menu.Item className="flex justify-start hover:bg-blue-200 p-1 rounded-lg">
+								{({ active }) => (
+
+									<div
+										className={`${active && 'hover'}`}
+
+									>
+										<LogoutIcon className="h-5 w-5 mr-1" />
+										Log out
+									</div>
+								)}
+							</Menu.Item>
+						</Link>
+					</Menu.Items>
+
+				</Menu>
 			</div>
-		</div>
+		</div >
 	);
 };
 
