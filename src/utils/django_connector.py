6607021bb433e541b14a305b520d978d908e46b2
Kabir <seth.kabir14@gmail.com>
2022-02-22 10:44:21 +0530

    customize frontend

--- src/utils/django_connector.py
+++ src/utils/django_connector.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_user_id(name):
-    student_url = 'http://localhost:8000/api/students'
+    student_url = 'http://localhost:8080/api/students'
     student_details = requests.get(student_url, data={'name': name}, auth=('a@a.com', 'admin'))
     # get id from json data
     # print(student_details.json()[0]['id'])
@@ -10,7 +10,7 @@ def get_user_id(name):
 
 def add_attendance(name):
     student_id = get_user_id(name)
-    attendance_url = 'http://localhost:8000/api/attendances'
+    attendance_url = 'http://localhost:8080/api/attendances'
     attendance_details = requests.post(attendance_url, data={'student_id': student_id}, auth=('a@a.com', 'admin'), headers={"Authorization": "Token 7b49e4ab9c734b0db015fe9eb3411652ffb9f8ce"})
     # print(attendance_details)
 
--- src/utils/utilities.py
+++ src/utils/utilities.py
@@ -4,7 +4,7 @@ import cv2
 import requests
 
 def get_user_id(name):
-    student_url = 'http://localhost:8000/api/students'
+    student_url = 'http://localhost:8080/api/students'
     student_details = requests.get(student_url, data={'name': name}, auth=('a@a.com', 'admin'))
     # get id from json data
     # print(student_details.json()[0]['id'])
@@ -12,7 +12,7 @@ def get_user_id(name):
 
 def add_attendance(name):
     student_id = get_user_id(name)
-    attendance_url = 'http://localhost:8000/api/attendances'
+    attendance_url = 'http://localhost:8080/api/attendances'
     attendance_details = requests.post(attendance_url, data={'student_id': student_id}, auth=('a@a.com', 'admin'), headers={"Authorization": "Token 7b49e4ab9c734b0db015fe9eb3411652ffb9f8ce"})
     # print(attendance_details)
 
@@ -70,3 +70,7 @@ def load_database(path):
         img = cv2.imread(f'{path}/{name}')
         database.append((os.path.splitext(name)[0], img))
     return database
+
+add_attendance("Shivang")
+add_attendance("Kabir")
+add_attendance("Sanchit")
